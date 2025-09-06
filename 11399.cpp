5e3ac9de692dd122a86bb45ef5ea26cd04541d44
펭귄꽝꽝 <pggg2222@gmail.com>
2025-09-06 15:05:09 +0000

    09-07

---
+++ 11399.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    vector<int> vec;
+    for (int i = 0; i < N; ++i) {
+        int M;
+        cin >> M;
+        vec.push_back(M);
+    }
+    sort(vec.begin(), vec.end());
+
+    int cnt = 0;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j <= i; ++j) {
+            cnt += vec[j];
+        }
+    }
+
+    cout << cnt;
+    return 0;
+}
