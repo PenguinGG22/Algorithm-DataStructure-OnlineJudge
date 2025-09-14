d86a5593cc146691bf598e2f5518b49d30e316cd
펭귄꽝꽝 <pggg2222@gmail.com>
2025-09-14 14:12:00 +0000

    Create : contents

---
+++ 25-09/11478.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){	
+	ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	
+    set<string> M;
+    string S = "";
+    cin >> S;
+    
+	int i,j,l=S.length();
+	
+	for(i=0;i<l;i++)
+		for(j=i;j<l;j++)
+			M.insert(S.substr(i,j-i+1));
+	
+	cout << M.size();
+}
