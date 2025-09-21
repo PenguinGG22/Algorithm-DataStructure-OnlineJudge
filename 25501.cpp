e780d591b5bcc8c11d8f9c75bf7d772171cee7e6
펭귄꽝꽝 <pggg2222@gmail.com>
2025-09-21 07:10:21 +0000

    Create : contents

---
+++ 25501.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cnt;
+
+int recursion(const char* s, int l, int r) {
+    cnt++;
+    if (l >= r) return 1;
+    else if (s[l] != s[r]) return 0;
+    else return recursion(s, l + 1, r - 1);
+}
+
+int isPalindrome(const char* s) {
+    return recursion(s, 0, strlen(s) - 1);
+}
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int T;
+	cin >> T;
+
+    while (T--) {
+        cnt = 0;
+        string str;
+        cin >> str;
+        cout << isPalindrome(str.c_str()) << ' ';
+        cout << cnt << '\n';
+    }
+
+    return 0;
+}
