a0ab897e6a9936931140064d4fb373f11e046b38
펭귄꽝꽝 <pggg2222@gmail.com>
2025-10-07 20:48:03 +0000

    Update : contents

---
+++ Ch.21/2579.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int s[305];
+int n;
+int d[305][3];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i)cin >> s[i];
+    if (n == 1) {
+        cout << s[1];
+        return 0;
+    }
+    
+    d[1][1] = s[1];
+    d[1][2] = 0;
+    d[2][1] = s[2];
+    d[2][2] = s[1] + s[2];
+    for (int i = 3; i <= n; ++i) {
+        d[i][1] = max(d[i - 2][1], d[i - 2][2]) + s[i];
+        d[i][2] = d[i - 1][1] + s[i];
+    }
+    cout << max(d[n][1], d[n][2]);
+    
+    return 0;
+}
+
+/*
+
+1. 테이블 정의
+D[i][j] = 현재까지 j개의 계단을 연속해서 밟고, i번째 계단까지 올라섰을때
+점수 합의 최댓값, 단 i번째 계단은 반드시 밟아야 함
+
+2. 점화식 찾기
+D[k][1] = max(D[k-2][1], D[k-2][2]) + S[k]
+D[k][2] = D[k-1][1] + S[k]
+
+3. 초기값 정의
+D[1][1] = S[1], D[1][2] = 0
+D[2][1] = S[2], D[2][2] = S[1] + S[2]
+
+*/
---
+++ Ch.23/11047.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	int* arr = new int[n];
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	int cnt = 0;
+	for (int i = n - 1; i >= 0; i--) {
+		if (k / arr[i] != 0) {
+			int quotient = k / arr[i];
+			cnt += quotient;
+			k = k - arr[i] * quotient;
+		}
+	}
+
+	cout << cnt;
+	delete[] arr;
+	return 0;
+}
