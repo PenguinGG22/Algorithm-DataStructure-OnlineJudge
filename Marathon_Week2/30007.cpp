5122aa5e5f552b3602b565b5b5df19d9302a3d69
펭귄꽝꽝 <pggg2222@gmail.com>
2025-10-07 21:18:46 +0000

    Update : contents

--- Marathon_Week2/30007.cpp
+++ Marathon_Week2/30007.cpp
@@ -4,14 +4,28 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    
-    int T;
-    cin >> T;
-    while (T--) {
-        int A, B, X;
-        cin >> A >> B >> X;
-        cout << A * (X - 1) + B << '\n';
+
+    long long N;
+    cin >> N;
+    vector<long long> sizes(6);
+
+    for (int i = 0; i < 6; i++) {
+        cin >> sizes[i];
     }
+
+    long long T, P;
+    cin >> T >> P;
+    long long total_shirt = 0;
+
+    for (int i = 0; i < 6; i++) {
+        total_shirt += (sizes[i] + T - 1) / T;
+    }
+
+    long long total_pen = N / P;
+    long long single_pen = N % P;
+
+    cout << total_shirt << '\n';
+    cout << total_pen << ' ' << single_pen;
 
     return 0;
 }
