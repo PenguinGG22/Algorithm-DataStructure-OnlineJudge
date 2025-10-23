ab9079523fdc5a8288472b060cf242e6581febe1
펭귄꽝꽝 <pggg2222@gmail.com>
2025-10-23 11:15:53 +0900

    Update 16394.cpp

--- Marathon_Week5_Cpp/16394.cpp
+++ Marathon_Week5_Cpp/16394.cpp
@@ -1,1 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int var;
+    cin >> var;
+    cout << var - 1946;
+
+    return 0;
+}
