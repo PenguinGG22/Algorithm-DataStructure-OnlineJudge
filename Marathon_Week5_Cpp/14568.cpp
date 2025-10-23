6e2ad7d4b0b67543457a17c7244168648a13326f
펭귄꽝꽝 <pggg2222@gmail.com>
2025-10-23 11:16:22 +0900

    Update 14568.cpp

--- Marathon_Week5_Cpp/14568.cpp
+++ Marathon_Week5_Cpp/14568.cpp
@@ -1,1 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int A = 0, B = 0, C = 0; // A:남규 B:영훈 C:택희
+    int N;
+    cin >> N;
+
+    int cnt = 0;
+    for (int i = 0; i <= N; i++) {
+        for (int j = 0; j <= N; j++) {
+            for (int k = 0; k <= N; k++) {
+                A = i; B = j; C = k;
+                if (A >= B + 2 && A != 0 && B != 0 && C != 0 && C % 2 == 0) {
+                    if (A + B + C == N) {
+                        cnt++;
+                    }
+                }
+            }
+        }
+    }
+    cout << cnt;
+
+    return 0;
+}
