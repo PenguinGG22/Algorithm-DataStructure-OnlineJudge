5f82218cc3a32cbd3bc9b31eb22a0e73938c9539
펭귄꽝꽝 <pggg2222@gmail.com>
2026-01-01 23:23:47 +0900

    Update : contents

---
+++ Marathon_Week6_Cpp/15680.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cin >> num;
+    if(num == 0){
+        cout << "YONSEI";
+    }
+    else{
+        cout << "Leading the Way to the Future";
+    }
+    return 0;
+}
