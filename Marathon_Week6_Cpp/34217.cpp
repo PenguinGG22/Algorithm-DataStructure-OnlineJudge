329ef45f8cde09874d7435e2f7afdcda85d9ae0c
펭귄꽝꽝 <pggg2222@gmail.com>
2026-01-01 23:31:41 +0900

    Update : contents

---
+++ Marathon_Week6_Cpp/34217.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int A, B, C, D;
+    cin >> A >> B >> C >> D;
+    if(A + C < B + D){
+        cout << "Hanyang Univ.";
+    }
+    else if(A + C > B + D){
+        cout << "Yongdap";
+    }
+    else{
+        cout << "Either";
+    }
+    return 0;
+}
